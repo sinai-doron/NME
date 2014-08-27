971c6cd39360de7d9e90400824a66c10444e014f
Doron Sinai <doron@Dorons-MacBook-Pro.local>
2014-08-27 11:44:08 +0300

    add express dependency

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var express = require('express');
 
 var db = mongoose.connection;
 
 db.on('error', console.error);
 db.once('open', function() {
   // Create your schemas and models here.
+  console.log('Connected to mongodb');
 });
 
 mongoose.connect('mongodb://localhost/test');
