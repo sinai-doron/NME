cbe0eda7b98909d34d463d6a9ddb016c737ada41
Doron Sinai <doron@ip-10-0-0-15.ec2.internal>
2014-08-30 12:54:49 +0300

    add zorse and its files including express

---
+++ zorse.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+var app = express();
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 5555);
+
+app.get('/', function(req, res) {
+        res.render('home');
+});
+
+app.get('/about', function(req, res){
+        res.type('text/plain');
+        res.send('About Meadowlark Travel');
+});
+
+app.use(function(req,res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Page Not Found');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express server started; press Ctrl+C to terminat me!');
+});
